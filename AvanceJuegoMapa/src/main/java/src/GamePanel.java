2db3fda06042e510ead2ad2e57e53bfba7ea94dd
FacundoDaniel8 <86319792+FacundoDaniel8@users.noreply.github.com>
2023-11-18 11:44:42 -0300

    Add files via upload

---
+++ AvanceJuegoMapa/src/main/java/src/GamePanel.java
@@ -0,0 +1,255 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+import sound.Sound;
+import entity.Dron;
+import entity.Hunter;
+import entity.Player;
+import static java.awt.Color.WHITE;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import tiles.TileManager;
+
+/**
+ *
+ * @author Alumno
+ */
+public class GamePanel extends JPanel implements Runnable {
+
+    // SCREEN SETTINGS
+    public final int tileSize = 64;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 10;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    // WOLRD SETTINGS
+    public final int maxWorldCol = 140;
+    public final int maxWorldRow = 40;
+    public final int wolrdWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+
+    public BufferedImage image;
+    public int direccion_image = 0;
+
+    public boolean respawn = false;
+
+    // FPS
+    int FPS = 60;
+
+    public TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    public ColisionChecker cChecker = new ColisionChecker(this);
+    public Player player = new Player(this, keyH);
+    public Dron dron = new Dron(this);
+    public Hunter hunter = new Hunter(this, keyH);
+    
+    //Si van a hacer un selector de nivel cuando quieran cambiar el nivel hagan que sea true esto y pongan el curlevel al nivel que quieran
+    
+    boolean winall=false;
+    boolean firstload=false;
+    public int curlevel=1;
+    
+    // public Sound sound = new Sound();
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+       
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            levelmanager();
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update() {
+
+        player.update();
+
+        if (player.muerte(hunter)) {
+
+            if (player.spriteDeath == 9) {
+                dron.setDefaultValues();
+                player.setDefaultValues();
+
+                player.spriteDeath = 1;
+
+                respawn = true;
+            }
+        }
+        if (respawn && player.spriteRespawn == 9) {
+
+            respawn = false;
+            player.spriteRespawn = 1;
+        }
+
+        dron.update();
+
+        hunter.update();
+    }
+    public void info(Graphics g){
+     Font fuente = new Font("Arial", Font.BOLD, 20);
+        g.setFont(fuente);
+        g.setColor(WHITE);
+        g.drawString("X: "+player.worldX+" Y: "+player.worldY, 0, 26);
+        g.drawString("Nivel: "+curlevel, screenWidth/2, 20);
+    
+    }
+    
+    //Se encarga de manejar que nivel carga el tilemanager si el jugador gana
+    public void levelmanager(){
+    //primera carga del nivel 1    
+    if(firstload==false){
+    tileM.loadMap("src/main/java/maps/map01.txt");
+    firstload=true;
+    }
+    
+    //si el jugador gana
+    if(player.victoria()==true ){
+            
+         //reinicia pocicion del jugador
+        player.setDefaultValues();
+            //aumenta el nivel
+            curlevel++;    
+            
+       
+        //carga cada nivel segun el nivel actual
+        switch(curlevel){
+            case 1:
+                tileM.loadMap("src/main/java/maps/map01.txt");
+             break;   
+        case 2:
+            tileM.loadMap("src/main/java/maps/map02.txt");
+         
+            break;
+        case 3:
+             tileM.loadMap("src/main/java/maps/map03.txt");
+            break;
+        case 4:
+            tileM.loadMap("src/main/java/maps/map04.txt");
+            break;
+        case 5:
+            curlevel=4;
+            winall=true;
+            break;    
+            
+            
+    }
+        
+
+    }
+    
+    
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        drawBackground(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        info(g2);
+      
+        tileM.draw(g2);
+        player.draw(g2);
+        dron.draw(g2);
+        hunter.draw(g2);
+
+        g2.dispose();
+    }
+
+    public void drawBackground(Graphics g) {
+
+        try {
+            Dimension tamanio = getSize();
+
+            File file = new File("src/main/java/imgFondo/backgroundSinArbolGrande.png");
+            FileInputStream fis = new FileInputStream(file);
+            this.image = ImageIO.read(fis);
+
+            g.drawImage(this.image, direccion_image, 0,
+                    tamanio.width, tamanio.height, this);
+            setOpaque(false);
+
+            g.drawImage(this.image, tamanio.width + direccion_image, 0,
+                    tamanio.width, tamanio.height, this);
+            setOpaque(false);
+
+            g.drawImage(this.image, -tamanio.width + direccion_image, 0,
+                    tamanio.width, tamanio.height, this);
+            setOpaque(false);
+            
+            g.drawImage(this.image, tamanio.width*2 + direccion_image, 0,
+                    tamanio.width, tamanio.height, this);
+            setOpaque(false);
+
+            if (keyH.right && !player.colisionLeft && !player.bloqueo) {
+                direccion_image--;
+
+            } else if (keyH.left && !player.colisionRight && !player.bloqueo) {
+                direccion_image++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    public void playMusic(int i) { //sera para la musica de fondo
+//        sound.setFile(i);
+//        sound.play();
+//        sound.loop();
+//    }
+//
+//    public void stopMusic() {//este detiene cualquier musica
+//        sound.stop();
+//    }
+//
+//    public void playSE(int i) {//este de aca se usa para efectos de audio
+//        sound.setFile(i);
+//        sound.play();
+//    }
+}
---
+++ AvanceJuegoMapa/src/main/java/src/KeyHandler.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package src;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Alumno
+ */
+public class KeyHandler implements KeyListener{
+
+    public boolean up, left, right, down;
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+        int code = e.getKeyCode();
+        
+        if (code == KeyEvent.VK_UP){
+            
+            up = true;
+        }
+        if (code == KeyEvent.VK_LEFT){
+            
+            left = true;
+        }
+        if (code == KeyEvent.VK_RIGHT){
+            
+            right = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+        int code = e.getKeyCode();
+        
+        if (code == KeyEvent.VK_UP){
+             
+            up = false;
+        }
+        if (code == KeyEvent.VK_LEFT){
+            
+            left = false;
+        }
+        if (code == KeyEvent.VK_RIGHT){
+            
+            right = false;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+    
+}
